330a44b2f7ee64e447c9f78851bba8b658370561
Fuad Kamilov <fuad.kamilov@scandiweb.com>
2024-12-15 22:54:11 +0400

    Starting to work on posts

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('posts', [PostController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
